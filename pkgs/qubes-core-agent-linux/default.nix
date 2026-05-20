a467c4b384be37e2da46fa0af481632d0e757fd6
FormindGCA <162144075+FormindGCA@users.noreply.github.com>
2026-05-20 11:41:03 +0100

    fix: update hash for qubes-core-agent-linux in default.nix

--- pkgs/qubes-core-agent-linux/default.nix
+++ pkgs/qubes-core-agent-linux/default.nix
@@ -5,7 +5,7 @@
 }:
 callPackage ./generic.nix {
   version = "4.3.43";
-  hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  hash = "sha256-9hWuAS2eijRQSw+Bx05TqDLSUPLyBpXK/tKLuHsWjjY=";
   inherit enableNetworking;
   rev = rev;
 }
