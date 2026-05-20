3aa73cc3e86f81e8067ee4acdda2f1ee86bf42fd
FormindGCA <162144075+FormindGCA@users.noreply.github.com>
2026-05-20 12:25:39 +0100

    fix: update hash for qubes-core-qrexec in default.nix

--- pkgs/qubes-core-qrexec/default.nix
+++ pkgs/qubes-core-qrexec/default.nix
@@ -1,6 +1,6 @@
 {callPackage, rev ? null}:
 callPackage ./generic.nix {
   version = "4.3.12";
-  hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  hash = "sha256-pG3tVc4AFYYu/7d4a1sfiGkSIu7cVp/YPW1oqcsgf9E=";
   rev = rev;
 }
