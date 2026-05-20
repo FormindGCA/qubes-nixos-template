efe923c07b892befd1d07dfd2b0387108a317c23
FormindGCA <162144075+FormindGCA@users.noreply.github.com>
2026-05-20 11:45:36 +0100

    fix: update hash for qubes-gui-agent-linux in default.nix

--- pkgs/qubes-gui-agent-linux/default.nix
+++ pkgs/qubes-gui-agent-linux/default.nix
@@ -1,6 +1,6 @@
 {callPackage, rev ? null}:
 callPackage ./generic.nix {
   version = "4.3.16";
-  hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+  hash = "sha256-k0GkwDABFmxTZHbK+xOF3t29mKZjH0ZzUhCmB1/Q5d4=";
   rev = rev;
 }
