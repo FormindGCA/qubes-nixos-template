46f4e338bf16ec261b2cdf7e339587125d8dd596
FormindGCA <162144075+FormindGCA@users.noreply.github.com>
2026-07-07 12:56:03 +0100

    Refactor qubes-sshd package

--- pkgs/qubes-sshd/default.nix
+++ pkgs/qubes-sshd/default.nix
@@ -1,13 +1,23 @@
 {
-  pkgs,
+  lib,
   stdenv,
+  socat,
+  writeTextFile,
 }:
-pkgs.writeTextFile {
+writeTextFile {
   name = "qubes-rpc-sshd";
   text = ''
     #!${stdenv.shell}
-    ${pkgs.socat}/bin/socat STDIO TCP:localhost:22
+    ${socat}/bin/socat STDIO TCP:localhost:22
   '';
   executable = true;
   destination = "/etc/qubes-rpc/qubes.Sshd";
+} // {
+  meta = with lib; {
+    description = "Qubes RPC action to proxy SSH connections to localhost:22 via qrexec";
+    homepage = "https://qubes-os.org";
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux;
+    maintainers = [];
+  };
 }
